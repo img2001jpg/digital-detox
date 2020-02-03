49d407b633f2151833b18d273600fbd6f489ce1b
Marco Land <marco.anmeldung@gmail.com>
2020-02-03 10:53:29 +0100

    add: dotenv / fix style url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,103 @@
-
 export default {
-  mode: 'spa',
+  mode: "spa",
   server: {
-    host: '0.0.0.0' // default: localhost
+    host: "0.0.0.0" // default: localhost
   },
   head: {
-    title: 'Digital Detox — Marco Land',
+    title: "Digital Detox — Marco Land",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Digital Detox' },
-      { name: 'msapplication-TileColor', content: '#ffffff' },
-      { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
-      { name: 'theme-color', content: '#ffffff' },
-      { property: 'og:image', content: 'https://digitaldetox.marco.land/og_image.jpg' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Digital Detox" },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+      { name: "theme-color", content: "#ffffff" },
+      {
+        property: "og:image",
+        content: "https://digitaldetox.marco.land/og_image.jpg"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/manifest.json' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "apple-touch-icon",
+        sizes: "57x57",
+        href: "/apple-icon-57x57.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/apple-icon-60x60.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        href: "/apple-icon-72x72.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/apple-icon-76x76.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "114x114",
+        href: "/apple-icon-114x114.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        href: "/apple-icon-120x120.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        href: "/apple-icon-144x144.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        href: "/apple-icon-152x152.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-icon-180x180.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/android-icon-192x192.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicon-96x96.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      { rel: "manifest", href: "/manifest.json" }
     ]
   },
-  loading: { color: '#fff' },
-  css: [
-    '~/assets/scss/main.scss'
-  ],
-  plugins: [
-    '~/plugins/components.js'
-  ],
-  buildModules: [
-    '@nuxtjs/eslint-module'
-  ],
-  modules: [
-    '@nuxtjs/axios'
-  ],
-  axios: {
-  },
+  loading: { color: "#fff" },
+  css: ["~/assets/scss/main.scss"],
+  plugins: ["~/plugins/components.js"],
+  buildModules: ["@nuxtjs/eslint-module", "@nuxtjs/dotenv"],
+  modules: ["@nuxtjs/axios"],
+  axios: {},
   build: {
-    extend (config, ctx) {
-    }
+    extend(config, ctx) {}
   }
-}
+};
